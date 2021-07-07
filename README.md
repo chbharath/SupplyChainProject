04042b3516cd8219cda089e8a33fa23df867a245
chbharath <chbharath@users.noreply.github.com>
2021-07-07 05:58:26 -0400

    Rinkeby deployment changes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+Truffle v5.3.12 (core: 5.3.12)
+Solidity v0.5.16 (solc-js)
+Node v10.16.0
+Web3.js v1.3.6
+
+
 # Supply chain & data auditing
 
 This repository containts an Ethereum DApp that demonstrates a Supply Chain flow between a Seller and Buyer. The user story is similar to any commonly used supply chain process. A Seller can add items to the inventory system stored in the blockchain. A Buyer can purchase such items from the inventory system. Additionally a Seller can mark an item as Shipped, and similarly a Buyer can mark an item as Received.
--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -18,8 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "b7c3bff7a19a482fb29b8661381615b4";
+const mnemonic = "sadness cross wheat coach cannon tourist degree offer top desert impact predict";
+
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -47,7 +49,13 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
       websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 21000000000
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
